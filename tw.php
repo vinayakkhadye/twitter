75116151f29bbf5c5f94a7585682c0e92d7dc757
vinayak <vinayak249@gmail.com>
2017-01-31 15:26:57 +0530

    message

---
+++ tw.php
@@ -0,0 +1,8 @@
+<?php
+require_once('twClass.php');
+$user_name	= $_GET['user_name'];
+$user_id	= $_GET['user_id'];
+$tw = new twitter($user_name,$user_id);
+$result = $tw->getBestDayAndTime();
+echo json_encode($result);
+?>
